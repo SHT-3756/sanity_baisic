725e3da5ad746a8c03b3d9a3e07442917eaf1ff8
SHT-3756 <gkdlfndkekd@naver.com>
2021-05-31 17:58:23 +0900

    'author스키마만들'

--- schemas/author.js
+++ schemas/author.js
@@ -7,15 +7,12 @@ export default {
       name: 'name',
       title: 'Name',
       type: 'string',
+      validation: (Rule) => Rule.required()
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 96,
-      },
+      name: 'role',
+      title: 'Role',
+      type: 'string'
     },
     {
       name: 'image',
@@ -24,20 +21,9 @@ export default {
       options: {
         hotspot: true,
       },
+      validation: (Rule) => Rule.required()
     },
-    {
-      name: 'bio',
-      title: 'Bio',
-      type: 'array',
-      of: [
-        {
-          title: 'Block',
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
-        },
-      ],
-    },
+   
   ],
   preview: {
     select: {
